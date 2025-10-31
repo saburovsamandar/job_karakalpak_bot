490784f35c6d6eae8db91758e94a70a3e974e921
saburovsamandar <saburovsamandar2004@gmail.com>
2025-10-31 18:08:52 +0500

    bot.py new language

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from aiogram.types import (
 from aiogram.filters import CommandStart
 from config import BotToken, ADMINS, CHANNEL_ID
 from lang import language
+from datetime import datetime
 
 # --- Bot va Dispatcher ---
 bot = Bot(token=BotToken)
@@ -16,8 +17,9 @@ dp = Dispatcher()
 # --- Inline va Reply menyular ---
 inline_menu = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='🇺🇿 Uzbek', callback_data='lang_uz')],
-        [InlineKeyboardButton(text='🇰🇿 Karakalpak', callback_data='lang_kk')],
+        [InlineKeyboardButton(text='🇺🇿 Uzbek tili', callback_data='lang_uz')],
+        [InlineKeyboardButton(text='🇬🇦 Qaraqalpaq tili', callback_data='lang_kk')],
+        [InlineKeyboardButton(text='🇷🇺 русский язык', callback_data='lang_ru')],
     ]
 )
 
@@ -36,7 +38,7 @@ waiting_for_admin = {}  # foydalanuvchi ma’lumotlari admin tasdiqlash uchun
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.answer(
-        f"{message.from_user.first_name}, iltimos tilni tanlang 👇",
+        f"{message.from_user.first_name}, Iltimas tildi saylań | iltimos tilni tanlang \n пожалуйста , выберите язык 👇",
         reply_markup=inline_menu
     )
 
@@ -51,7 +53,7 @@ async def set_language(callback: CallbackQuery):
     await callback.answer()
 
 # --- Vakansiya qo‘shish ---
-@dp.message(F.text.in_([language['uz']['vakansiya'], language['kk']['vakansiya']]))
+@dp.message(F.text.in_([language['uz']['vakansiya'], language['kk']['vakansiya'],language['ru']['vakansiya']]))
 async def start_vacancy(message: Message):
     uid = message.from_user.id
     lang = user_language.get(uid)
@@ -87,8 +89,8 @@ async def show_user_preview(message: Message, data, lang):
     if data.get('is_admin'):  # Admin vakansiyasi
         markup = InlineKeyboardMarkup(
             inline_keyboard=[
-                [InlineKeyboardButton(text="✅ Kanalga joylash", callback_data=f"admin_self_approve_{message.from_user.id}")],
-                [InlineKeyboardButton(text="❌ Bekor qilish", callback_data=f"admin_self_cancel_{message.from_user.id}")]
+                [InlineKeyboardButton(text="✅ Kanalǵa jaylaw", callback_data=f"admin_self_approve_{message.from_user.id}")],
+                [InlineKeyboardButton(text="❌ Bıykarlaw", callback_data=f"admin_self_cancel_{message.from_user.id}")]
             ]
         )
     else:  # Foydalanuvchi vakansiyasi
@@ -112,18 +114,46 @@ async def user_send(callback: CallbackQuery):
     text = language[data['lang']]['info_text'].format(**data)
     markup_admin = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="✅ Kanalga joylash", callback_data=f"admin_approve_{uid}")],
-            [InlineKeyboardButton(text="❌ Bekor qilish", callback_data=f"admin_cancel_{uid}")]
+            [InlineKeyboardButton(text="✅ Kanalǵa jaylaw", callback_data=f"admin_approve_{uid}")],
+            [InlineKeyboardButton(text="❌ Bıykarlaw", callback_data=f"admin_cancel_{uid}")]
         ]
     )
 
     # Faqat adminlarga yuboriladi
     for admin_id in ADMINS:
-        await bot.send_message(admin_id, f"📥 Yangi vakansiya keldi:\n\n{text}", reply_markup=markup_admin)
+        text = language[data['lang']]['info_text'].format(**data)
+        clean_text = ''
+        if "✅ Siz kirgizgen maǵlıwmatlar:\n\n" in text:
+            clean_text = text.replace("✅ Siz kirgizgen maǵlıwmatlar:\n\n", "")
+        elif "✅ Siz kiritgan maʼlumotlar:\n\n" in text:
+            clean_text = text.replace("✅ Siz kiritgan maʼlumotlar:\n\n", "")
+        elif "✅ Введенные вами данные:\n\n" in text:
+            clean_text = text.replace("✅ Введенные вами данные:\n\n", "")   
+            # uz lang 
+        if "Lavozim:" in clean_text:
+            clean_text = clean_text.replace("Lavozim:", "<b>👨‍💼 Lawazım:</b>")
+            clean_text = clean_text.replace("Tashkilot:", "<b>🏛 Mekeme:</b>")
+            clean_text = clean_text.replace("Manzil:", "<b>📍 Mánzil:</b>")
+            clean_text = clean_text.replace("Aloqalar:", "<b>☎️ Baylanıs:</b>")
+            clean_text = clean_text.replace("Ish haqi:", "<b>💰 Aylıq:</b>")
+            clean_text = clean_text.replace("Ish vaqti:", "<b>⏰ Jumıs waqıtı:</b>")
+            clean_text = clean_text.replace("Talablar:", "<b>📌 Talaplar:</b>")
+            clean_text = clean_text.replace("Qo'shimcha:", "<b>📎Qosımsha:</b>")
+        elif "Должность" in clean_text:
+            clean_text = clean_text.replace("Должность:", "<b>👨‍💼 Lawazım:</b>")
+            clean_text = clean_text.replace("Организация:", "<b>🏛 Mekeme:</b>")
+            clean_text = clean_text.replace("Местонахождение:", "<b>📍 Mánzil:</b>")
+            clean_text = clean_text.replace("Требования:", "<b>📌 Talaplar:</b>")
+            clean_text = clean_text.replace("Час работы:", "<b>⏰ Jumıs waqıtı:</b>")
+            clean_text = clean_text.replace("Контакты:", "<b>☎️ Baylanıs:</b>")
+            clean_text = clean_text.replace("Зарплата:", "<b>💰 Aylıq:</b>")
+            clean_text = clean_text.replace("Дополнительные:", "<b>📎Qosımsha:</b>")
+        print(clean_text)
+        await bot.send_message(admin_id, f"📥 Jańa vakansiya keldi:\n\n{clean_text}", reply_markup=markup_admin,parse_mode="HTML")
 
     waiting_for_admin[uid] = data.copy()
     await callback.message.delete()
-    await callback.message.answer("✅ Vakansiya adminlarga yuborildi!", reply_markup=make_reply_keyboard(data['lang']))
+    await callback.message.answer(f"{language[data['lang']]['final_send']}", reply_markup=make_reply_keyboard(data['lang']))
     del user_data[uid]
 
 # --- Admin (foydalanuvchi vakansiyasi) tasdiqlasa ---
@@ -132,15 +162,41 @@ async def admin_approve(callback: CallbackQuery):
     uid = int(callback.data.split("_")[-1])
     data = waiting_for_admin.get(uid)
     if not data:
-        await callback.answer("❗️ Ma'lumot topilmadi yoki allaqachon qayta ishlangan.", show_alert=True)
+        await callback.answer("❗️ Maǵlıwmat tabılmadı yamasa álleqashan qayta islengen.", show_alert=True)
         return
 
     text = language[data['lang']]['info_text'].format(**data)
-    clean_text = text.replace("✅ Siz kiritgan maʼlumotlar:\n\n", "")
-    await bot.send_message(CHANNEL_ID, f"📢 Diqqat, yangi vakansiya!\n\n{clean_text}")
+    clean_text = ''
+    if "✅ Siz kirgizgen maǵlıwmatlar:\n\n" in text:
+        clean_text = text.replace("✅ Siz kirgizgen maǵlıwmatlar:\n\n", "")
+    elif "✅ Siz kiritgan maʼlumotlar:\n\n" in text:
+        clean_text = text.replace("✅ Siz kiritgan maʼlumotlar:\n\n", "")
+    elif "✅ Введенные вами данные:\n\n" in text:
+        clean_text = text.replace("✅ Введенные вами данные:\n\n", "")
+        # uz lang 
+    if "Lavozim:" in clean_text:
+        clean_text = clean_text.replace("Lavozim:", "<b>👨‍💼 Lawazım:</b>")
+        clean_text = clean_text.replace("Tashkilot:", "<b>🏛 Mekeme:</b>")
+        clean_text = clean_text.replace("Manzil:", "<b>📍 Mánzil:</b>")
+        clean_text = clean_text.replace("Aloqalar:", "<b>☎️ Baylanıs:</b>")
+        clean_text = clean_text.replace("Ish haqi:", "<b>💰 Aylıq:</b>")
+        clean_text = clean_text.replace("Ish vaqti:", "<b>⏰ Jumıs waqıtı:</b>")
+        clean_text = clean_text.replace("Talablar:", "<b>📌 Talaplar:</b>")
+        clean_text = clean_text.replace("Qo'shimcha:", "<b>📎Qosımsha:</b>")
+    elif "Должность" in clean_text:
+        clean_text = clean_text.replace("Должность:", "<b>👨‍💼 Lawazım:</b>")
+        clean_text = clean_text.replace("Организация:", "<b>🏛 Mekeme:</b>")
+        clean_text = clean_text.replace("Местонахождение:", "<b>📍 Mánzil:</b>")
+        clean_text = clean_text.replace("Требования:", "<b>📌 Talaplar:</b>")
+        clean_text = clean_text.replace("Час работы:", "<b>⏰ Jumıs waqıtı:</b>")
+        clean_text = clean_text.replace("Контакты:", "<b>☎️ Baylanıs:</b>")
+        clean_text = clean_text.replace("Зарплата:", "<b>💰 Aylıq:</b>")
+        clean_text = clean_text.replace("Дополнительные:", "<b>📎Qosımsha:</b>")
+    print(clean_text)
+    await bot.send_message(CHANNEL_ID, f"<b>📢 DIQQAT, TAZA VAKANCIYA!</b>\n\n{clean_text}\n\n\n<b>📅 Jaylastırılǵan sáne:</b>  {datetime.now().strftime("%Y.%m.%d")}",parse_mode="HTML")
 
     await callback.message.delete()
-    await bot.send_message(uid, "✅ Sizning vakansiyangiz kanalga joylandi!", reply_markup=make_reply_keyboard(data['lang']))
+    await bot.send_message(uid, f"{language[data['lang']]['approved']}", reply_markup=make_reply_keyboard(data['lang']))
     del waiting_for_admin[uid]
 
 # --- Admin (foydalanuvchi vakansiyasi) bekor qilsa ---
@@ -150,9 +206,9 @@ async def admin_cancel(callback: CallbackQuery):
     data = waiting_for_admin.get(uid)
     await callback.message.delete()
     lang = user_language.get(callback.from_user.id, 'uz')
-    await callback.message.answer("❌ Vakansiya bekor qilindi.", reply_markup=make_reply_keyboard(lang))
+    await callback.message.answer("❌ Vakansiya biykar etildi.", reply_markup=make_reply_keyboard(lang))
     if data:
-        await bot.send_message(uid, "❌ Siz yuborgan vakansiya bekor qilindi.", reply_markup=make_reply_keyboard(data['lang']))
+        await bot.send_message(uid, f"{language[data['lang']]['rejected']}", reply_markup=make_reply_keyboard(data['lang']))
         del waiting_for_admin[uid]
 
 # --- Admin (o‘zi vakansiya qo‘shgan) tasdiqlasa ---
@@ -161,24 +217,24 @@ async def admin_self_approve(callback: CallbackQuery):
     uid = int(callback.data.split("_")[-1])
     data = user_data.get(uid)
     if not data:
-        await callback.answer("❗️ Ma'lumot topilmadi.", show_alert=True)
+        await callback.answer("❗️ Maǵlıwmat tabılmadı.", show_alert=True)
         return
 
     # Admin vakansiyasi uchun alohida text
     clean_text = (
-        f"Lavozim: {data['position']}\n"
-        f"Tashkilot: {data['organization']}\n"
-        f"Manzil: {data['address']}\n"
-        f"Talablar: {data['requirements']}\n"
-        f"Ish vaqti: {data['working_hours']}\n"
-        f"Oylik: {data['salary']}\n"
-        f"Aloqalar: {data['contacts']}\n"
-        f"Qo'shimcha: {data['additional']}"
+        f"<b>👨‍💼 Lawazım:</b> {data['position']}\n"
+        f"<b>🏛 Mekeme:</b> {data['organization']}\n"
+        f"<b>📍 Mánzil:</b> {data['address']}\n"
+        f"<b>📌 Talaplar:</b> {data['requirements']}\n"
+        f"<b>⏰ Jumıs waqıtı:</b> {data['working_hours']}\n"
+        f"<b>💰 Aylıq:</b> {data['salary']}\n"
+        f"<b>☎️ Baylanıs:</b> {data['contacts']}\n"
+        f"<b>📎Qosımsha:</b> {data['additional']}"
     )
 
-    await bot.send_message(CHANNEL_ID, f"📢 Diqqat, taza vakansiya \n\n{clean_text}")
+    await bot.send_message(CHANNEL_ID, f"<b>📢 DIQQAT, TAZA VAKANCIYA!</b> \n\n{clean_text}\n\n\n<b>📅 Jaylastırılǵan sáne:</b>  {datetime.now().strftime("%Y.%m.%d")}",parse_mode="HTML")
     await callback.message.delete()
-    await callback.message.answer("✅ Vakansiya kanalga joylandi!", reply_markup=make_reply_keyboard(data['lang']))
+    await callback.message.answer("✅ Vakansiya kanalǵa jaylastırıldı!", reply_markup=make_reply_keyboard(data['lang']))
     del user_data[uid]
 
 # --- Admin (o‘zi vakansiya qo‘shgan) bekor qilsa ---
@@ -188,7 +244,7 @@ async def admin_self_cancel(callback: CallbackQuery):
     data = user_data.get(uid)
     await callback.message.delete()
     if data:
-        await callback.message.answer("❌ Vakansiya bekor qilindi.", reply_markup=make_reply_keyboard(data['lang']))
+        await callback.message.answer("❌ Vakansiya biykar etildi.", reply_markup=make_reply_keyboard(data['lang']))
         del user_data[uid]
 
 # --- Qayta to‘ldirish ---
--- lang.py
+++ lang.py
@@ -3,34 +3,53 @@
 language = {
     'uz':{
         'start':'Assalamu aleykum tilni tanlang',
-        'chosen':'siz uzbek tilni tanladingiz',
-        'vakansiya':"vakansiya qo'shish",
-        'send':"Jo'natish",
+        'chosen':'Siz uzbek tilni tanladingiz',
+        'vakansiya':"➕ Vakansiya qo'shish",
+        'send':"✅ Jo'natish",
         'fill':'🔁Vakansiyani boshidan to‘ldirish',
         "questions": [
                 "1. Lavozim:", "2. Tashkilot:", "3. Manzil:", "4. Talablar:",
-                "5. Ish vaqti:", "6. Oylik:", "7. Aloqalar:", "8. Qo'shimcha:"
+                "5. Ish vaqti:", "6. Ish haqi:", "7. Aloqalar:", "8. Qo'shimcha:"
             ],
-        'info_text': '✅ Siz kiritgan maʼlumotlar:\n\nLavozim: {position}\nTashkilot: {organization}\nManzil: {address}\nTalablar: {requirements}\nIsh vaqti: {working_hours}\nOylik: {salary}\nAloqalar: {contacts}\nQo\'shimcha: {additional}',
+        'not_data':'❗️ Ma\'lumot topilmadi, qaytadan boshlang.',
+        'info_text': '✅ Siz kiritgan maʼlumotlar:\n\nLavozim: {position}\nTashkilot: {organization}\nManzil: {address}\nTalablar: {requirements}\nIsh vaqti: {working_hours}\nIsh haqi: {salary}\nAloqalar: {contacts}\nQo\'shimcha: {additional}',
         'final_send': "✅ Sizning vakansiyangiz yuborildi. Adminlar tekshirib chiqishadi.",
-        'sent': "📨 Vakansiya yuborildi. Admin tasdiqlashi kutilmoqda.",
+        'admin_txt':"📥 Yangi vakansiya keldi:",
         'approved': "✅ Sizning vakansiyangiz qabul qilindi va kanalga joylandi!",
         'rejected': "❌ Afsuski, sizning vakansiyangiz bekor qilindi."
     },
     'kk':{
         'start':'Assalawma aleykum tildi tanlan',
-        'chosen':'siz karakalpak tildi tanladingiz',
-        'vakansiya':"vakansiya qosiw",
-        'send':"Jıberiw",
+        'chosen':'Siz qaraqalpaq tildi tanladingiz',
+        'vakansiya':"➕ Vakansiya qosiw",
+        'send':"✅ Jıberiw",
         'fill':'🔁vakansiyani qayta toltiriw',
         "questions": [
                 "1. Lawazım:", "2. Mekeme:", "3. Mánzil:", "4. Talaplar:",
                 "5. Jumıs waqıtı:", "6. Aylıq:", "7. Baylanıs:", "8. Qosımsha:"
             ],
-        'info_text': '✅ Siz kiritgan maʼlumotlar:\n\nLawazım: {position}\nMekeme: {organization}\nMánzil: {address}\nTalaplar: {requirements}\nJumıs waqıtı: {working_hours}\nAylıq: {salary}\nBaylanıs: {contacts}\nQosımsha: {additional}',
-        'final_send': "✅ Sizning vakansiyangiz yuborildi. Adminlar tekshirib chiqishadi.",
-        'sent': "📨 Vakansiya yuborildi. Admin tasdiqlashi kutilmoqda.",
-        'approved': "✅ Sizning vakansiyangiz qabul qilindi va kanalga joylandi!",
-        'rejected': "❌ Afsuski, sizning vakansiyangiz bekor qilindi."
-    }
+        'not_data':'❗️ Maǵlıwmat tabılmadı, qaytaldan baslan\'',
+        'info_text': '✅ Siz kirgizgen maǵlıwmatlar:\n\n👨‍💼 Lawazım: {position}\n🏛 Mekeme: {organization}\n📍 Mánzil: {address}\n📌 Talaplar: {requirements}\n⏰ Jumıs waqıtı: {working_hours}\n💰 Aylıq: {salary}\n☎️ Baylanıs: {contacts}\n📎 Qosımsha: {additional}',
+        'final_send': "✅ Sizdiń vakansiyańız jiberildi. Admininlar tekserip shıǵadı.",
+        'admin_txt':"📥 Jańa vakansiya keldi:",
+        'approved': "✅ Sizdiń vakansiyańız qabıllandı hám kanalǵa jaylastırıldı!",
+        'rejected': "❌ Ókiniw menen aytamız, sizdiń vakansiyańız biykar etildi."
+    },
+    'ru':{
+        'start':'Assalamu aleykum tilni tanlang',
+        'chosen':'Вы выбрали русский язык',
+        'vakansiya':"➕ Добавить вакансию",
+        'send':"✅ Отправить",
+        'fill':'🔁Заполнение вакансии заново',
+        "questions": [
+                "1. Должность:", "2. Организация:", "3. Местонахождение:", "4. Требования:",
+                "5. Час работы:", "6. Зарплата:", "7. Контакты:", "8. Дополнительные:"
+            ],
+        'not_data':'❗️ Данные не найдены, попробуйте заново.',
+        'info_text': '✅ Введенные вами данные:\n\nДолжность: {position}\nОрганизация: {organization}\nМестонахождение: {address}\nТребования: {requirements}\nЧас работы: {working_hours}\nЗарплата: {salary}\nКонтакты: {contacts}\nДополнительные: {additional}',
+        'final_send': "✅ Ваша вакансия отправлена  Администраторы проверят это",
+        'admin_txt':"📥 Новая вакансия:",
+        'approved': "✅ Ваша вакансия была принята и размещена на канале!",
+        'rejected': "❌ К сожалению, ваша вакансия отменена"
+    },
 }
